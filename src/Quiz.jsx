0a547a699e4250ea1dc34ca33e33b1c378dcd926
prio101 <miprio101@gmail.com>
2021-10-15 01:48:12 +0000

    progress: adding answers

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import QuizAnswer from './QuizAnswer';
+import Question from './Question';
 
 class Quiz extends React.Component {
   constructor(props){
@@ -8,18 +10,27 @@ class Quiz extends React.Component {
       id: this.props.id,
       quiz: {},
       loaded: false,
-      questions: []
+      questions: [],
+      answers: []
     }
   }
 
   componentDidMount(){
     fetch(`http://localhost:3000/api/v1/quizes/${this.state.id}`)
-    .then(res=> res.json())
-    .then(res => this.setState({ quiz: res, questions: res.questions }))
+    .then(res => res.json())
+    .then(res => this.setState({ quiz: res, 
+                                 questions: res.questions }))
     this.setState({ loaded: true });
   }
 
+  onAnswer = (id, answer) =>{
+    this.setState({
+      answers: this.state.answers.concat({ [id]: answer })
+    })
+  }
+
   render(){
+    console.log(this.state.answers)
     return(
       <>
         {this.state.loaded ? 
@@ -32,22 +43,10 @@ class Quiz extends React.Component {
             {this.state.questions.map(q => {
               return(
                 <>
-                  <div key={q.id} 
-                        className="text-xl font-bold">
-                    {q.id}. {q.question}
-                    
-                    <div className="flex flex-col">
-                      {Object.keys(q.options).map(option => {
-                        return(
-                          <>
-                            <div>
-                              {option}: {q.options[option]}
-                            </div> 
-                          </>
-                        )
-                      })}
-                    </div>
-                  </div> 
+                  <Question question={q} />  
+                  <QuizAnswer question={q} 
+                              quizCallBack={this.onAnswer}
+                              />
                 </>
               )
             })}
